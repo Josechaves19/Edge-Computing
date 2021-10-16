322157782ae0de512d5c7379c19e980e28d92d19
Chaves <joseandreschw21@gmail.com>
2021-10-16 14:40:42 -0600

    Codigo utilizando TFLite

---
+++ EmotionsTFLite.py
@@ -0,0 +1,60 @@
+import numpy as np
+import cv2
+import datetime
+import time
+import tflite_runtime.interpreter as tflite
+
+Interpreter=tflite.Interpreter(model_path="model.tflite")
+Interpreter.allocate_tensors()
+
+input_details=Interpreter.get_input_details()
+output_details=Interpreter.get_output_details()
+
+
+# prevents openCL usage and unnecessary logging messages
+cv2.ocl.setUseOpenCL(False)
+
+# dictionary which assigns each label an emotion (alphabetical order)
+emotion_dict = {0: "Enojado", 1: "Disgustado", 2: "Temeroso", 3: "Feliz", 4: "Neutral", 5: "Triste", 6: "Sorprendido"}
+
+# start the webcam feed
+cap = cv2.VideoCapture(0)
+Emotions_File = open("Emotions_File.csv", "a") # Abre archivo de texto
+while True:
+    # Find haar cascade to draw bounding box around face
+    ret, frame = cap.read()
+    if not ret:
+        break
+    facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 255, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        cropped_img=np.array(cropped_img, dtype='f')
+        Interpreter.set_tensor(input_details[0]['index'], cropped_img)
+        Interpreter.invoke()
+        output_data = Interpreter.get_tensor(output_details[0]['index'])
+        maxindex = int(np.argmax(output_data))
+        cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (200, 0, 0), 2, cv2.LINE_AA) # Texto de la emoción
+
+        # ct stores current time
+        ct = datetime.datetime.now()
+
+        # ts store timestamp of current time
+        ts = time.time()
+        
+        emocion = emotion_dict[maxindex]
+        # print("Emoción: ", emocion,",Datetime:", ct,",Timestamp:", ts)
+        if ts % 5 <0.1:
+            Emotions_File.write(str((emocion))+";"+str(ct)+";"+str(ts)+"\n") # Guardar las emociones en un .txt
+            
+    cv2.imshow('Video', cv2.resize(frame,(800,480),interpolation = cv2.INTER_CUBIC)) # Ventana de video
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+Emotions_File.close() # Cierra archivo de texto
+cap.release()
+cv2.destroyAllWindows()
